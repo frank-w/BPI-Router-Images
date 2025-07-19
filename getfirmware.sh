f5160a5a256996ec2955ff50c643a42a63d4d31f
Frank Wunderlich <frank-w@public-files.de>
2025-07-19 09:53:55 +0200

    getfirmware.sh: update to update folder and more vendors

--- getfirmware.sh
+++ getfirmware.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 localdir=firmware
-folders="mediatek"
+folders="aeonsemi airoha mediatek"
 git clone -n --depth=1 --filter=tree:0 \
   https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git \
   $localdir
 cd $localdir
+git fetch --depth=1
+git reset --hard origin/main
 git sparse-checkout set --no-cone $folders
 git checkout
 du -hs
